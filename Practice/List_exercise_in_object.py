7b5d1164cb0c2cda4dddf1de6342f98cb2cfe7e1
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-23 06:12:34 -0700

    .

---
+++ Practice/List_exercise_in_object.py
@@ -0,0 +1,16 @@
+"""
+student_score1 = 50
+student_score1 = 50
+student_score1 = 50
+student_score1 = 50
+"""
+
+#do this
+#	  0   1   2   3    4
+scores = [50, 34, 45, 50, 25]
+
+cart = ['banana', 33, 'juice', 2.5, ["fish", "palm oil"], True]
+print(len(cart))
+
+print("we have", len(scores), "scores")
+print(scores[2])
