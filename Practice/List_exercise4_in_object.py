10ac5e650654d852b722552558fcdcd19a25043a
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-23 06:06:10 -0700

    .

---
+++ Practice/List_exercise4_in_object.py
@@ -0,0 +1,22 @@
+"""
+student_score1 = 50
+student_score1 = 50
+student_score1 = 50
+student_score1 = 50
+"""
+
+#do this
+#	  0   1   2   3    4
+scores = [50, 34, 45, 50, 25]
+
+cart = ['banana', 33, 'juice', 2.5, ["fish", "palm oil"], True]
+print(cart[4][1])
+
+print("we have", len(scores), "scores")
+print(scores[2])
+
+for score in scores:
+	print(score)
+
+for index in range(len(scores)):
+	print(index)
