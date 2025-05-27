cfa864a1322ed99e7ef4674e702f4dcd2ce631ce
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-27 06:18:33 -0700

    .

---
+++ Phase Gate/encrypt_test/function.py
@@ -0,0 +1,16 @@
+user_input = input('Enter a letter: ')
+
+cart = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+print(cart[][])
+
+def get_encrypt_text(user_input):
+
+	for letter in letters:
+		print(letter)
+
+	for user_input in range(len(letter)):
+		print(user_input)
+
+	for user_input in enumerate(cart):
+		print(letter)
+	print(list(enumerate(cart)))
