fb393405189d8fc998cd2fca7ba61806c96c6cdb
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-27 06:59:49 -0700

    Add files via upload

--- Phase Gate/encrypt_test/function.py
+++ Phase Gate/encrypt_test/function.py
@@ -1,14 +1,14 @@
 user_input = input('Enter a letter: ')
 
 cart = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-print(cart[][])
+print(cart" ", " ")
 
 def get_encrypt_text(user_input):
 
 	for letter in letters:
 		print(letter)
 
-	for user_input in range(len(letter)):
+	for user_input in range(len(cart)):
 		print(user_input)
 
 	for user_input in enumerate(cart):
