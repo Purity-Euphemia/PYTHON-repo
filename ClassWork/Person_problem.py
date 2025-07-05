83017e3475c25c27b1377bbf2a4333f383ac68aa
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-07-05 09:43:34 +0100

    Add files via upload

---
+++ ClassWork/Person_problem.py
@@ -0,0 +1,19 @@
+from problem import  Problem
+
+
+class Person:
+    def __init__(self, name: str, age: int, gender: str):
+        self.__name = name # is now only accessible within the class(private)
+        self.__age = age
+        self.gender = gender
+        self.__problems = []
+
+    def add_problem(self, problem: Problem):
+        self.__problems.append(problem)
+
+
+
+p1 = Person("john",22,"Male")
+
+
+
---
+++ ClassWork/problem.py
@@ -0,0 +1,8 @@
+class Problem:
+    def __init__(self, name,brand):
+        self.name = name
+        self.brand = brand
+        self.is_solved = False
+
+    def solve(self,name):
+        ...
