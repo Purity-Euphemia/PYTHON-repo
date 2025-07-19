9d5d1c84eb0c0c68bbf8104dc6509e40be924930
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-07-19 02:05:17 +0100

    Add files via upload

--- TDD_TEST/human_class/src/DSA/man_class.py
+++ TDD_TEST/human_class/src/DSA/man_class.py
@@ -3,3 +3,15 @@ from Human_Being import Human_Being
 class Man(Human_Being):
     def speak(self):
         return f"My name is {self.get_name()}, and I am a man"
+
+    def work(self):
+        return f"My name is {self.get_name()}, is working as an engineer"
+
+    def walk(self):
+        return f"My name is {self.get_name()}, is walking to the park"
+
+    def sleep(self):
+        return f"My name is {self.get_name()}, is sleeping"
+
+    def introduce(self):
+        return f"Hello, i am {self.get_name()}, a{self.get_age()} years old"
--- TDD_TEST/human_class/src/DSA/woman_class.py
+++ TDD_TEST/human_class/src/DSA/woman_class.py
@@ -4,3 +4,14 @@ class Woman(Human_Being):
     def speak(self):
         return f"My name is {self.get_name()}, and I am a woman"
 
+    def work(self):
+        return f"{self.get_name()}, is working as a doctor"
+
+    def walk(self):
+        return f"{self.get_name()}, is walking through the city"
+
+    def sleep(self):
+        return f"{self.get_name()}, is getting some sleep"
+
+    def introduce(self):
+        return f"Hello, I am {self.get_name()}, a{self.get_age()} years old"
