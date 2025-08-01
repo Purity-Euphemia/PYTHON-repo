30e04a011a6119948a11deb12e25c18b8f7fb379
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-01 05:11:29 +0100

    Add files via upload

--- TDD_TEST/Medical_Record_App/src/DSA/main.py
+++ TDD_TEST/Medical_Record_App/src/DSA/main.py
@@ -1,4 +1,6 @@
+from Doctor import Doctor
 from MedicalRecordsSystem import MedicalRecordsSystem
+from Patient import Patient
 
 MedicalRecordsSystem = MedicalRecordsSystem()
 
@@ -19,3 +21,28 @@ while True:
         patientName = input("Enter Patient Name: ")
         patientDateOfBirth = input("Enter Patient Date of Birth(YYYY-MM-DD): ")
         patientContact = input("Enter Patient Contact: ")
+        patientAilment = input("Enter Patient Ailment: ")
+        MedicalRecordsSystem.add_patient(Patient(patientID, patientName, patientDateOfBirth, patientContact, patientAilment))
+        print("Patient Registration Complete")
+
+    elif choice == "2":
+        doctorID = input("Enter Doctor ID: ")
+        doctorName = input("Enter Doctor Name: ")
+        doctorSpecilization = input("Enter Doctor Specilization: ")
+        doctorContact = input("Enter Doctor Contact: ")
+        doctorShift = input("Enter Doctor Shift: ")
+        MedicalRecordsSystem.add_doctor(Doctor(doctorID, doctorName, doctorSpecilization, doctorContact, doctorShift))
+        print("Doctor Registration Complete")
+
+    elif choice == "3":
+        appointmentID = input("Enter Appointment ID: ")
+        patientID = input("Enter Patient ID: ")
+        appointmentDateAndTime = input("Enter Appointment Date and Time: ")
+        appointmentShift = input("Enter Appointment Shift: ")
+        try:
+            appointment = MedicalRecordsSystem.schedule_auto_appointment(appointmentID, appointmentDateAndTime, appointmentShift)
+            print("Appointment Scheduled: ")
+            print(appointment.get_details())
+        except Exception as e:
+            print("Error: ", e)
+
