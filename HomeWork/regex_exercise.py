9d5004bcdb9d6c3b517805c8c0079ddba074bc4c
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-09 04:10:34 +0100

    Add files via upload

--- HomeWork/regex_exercise.py
+++ HomeWork/regex_exercise.py
@@ -4,11 +4,11 @@ pattern1 = re.findall(r'\d{3}-\d{3}-\d{4}', numbers)
 print(pattern1)
 
 email = "tolu@gmail.com"
-pattern2 = re.findall(r'([a-z@0-9a-z.a-z]+)', email)
+pattern2 = re.findall(r'(^[\w@][a-z]+@gmail|yahoomail\.[a-z]+)', email)
 print(pattern2)
 
 text = "Alice and Bob are Good Friends"
-pattern = re.findall(r'[A-Z]\w+', text)
+pattern = re.findall(r'\b[A-Z][a-z]+\b', text)
 print(pattern)
 
 text = "Hello! How are you doing?"
