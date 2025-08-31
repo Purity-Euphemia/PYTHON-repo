68b1e1a40a1e9ce33c4f54d288dce8dec1ad7183
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-31 20:24:18 +0100

    Add files via upload

---
+++ Practice/new_element.py
@@ -0,0 +1,10 @@
+def new_element(array1, array2):
+	result = array1 + array2
+	return result
+
+value1 = [1,2,3,4,5]
+value2 = [6,7,8,9,10]
+
+sum_array = new_element(value1, value2)
+print(sum_array)
+	
