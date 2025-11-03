0d229532ddb6c6c7c30d9524b7f06fb4e04f5a1a
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-03 23:19:54 +0100

    Add files via upload

---
+++ Practice/Student Information.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Alice Johnson",
+    "age": 21,
+    "grade": "A",
+    "major": "Computer Science",
+    "is_enrolled": True
+}
+
+print(student)
+print(student["major"])
