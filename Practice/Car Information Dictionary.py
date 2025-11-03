e0bd6833a0c819828f7b9411433600364d66dfbc
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-03 23:22:45 +0100

    Add files via upload

---
+++ Practice/Car Information Dictionary.py
@@ -0,0 +1,17 @@
+car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2022,
+    "color": "Blue",
+    "is_electric": False
+}
+
+print(car)
+print(car["model"])     
+
+car["color"] = "Red"
+
+
+car["mileage_km"] = 15000
+
+print(car)
